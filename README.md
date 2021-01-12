60cc219e2e58a528c45beea6cf8b26327fdc3ab5
Brian Park <briancpark@berkeley.edu>
2021-01-12 15:32:27 -0500

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # setup
-Brian's personal setup and configurations for Linux
+Installations of ALL my essentials! No need to waste time when I fire up a new VM or install a new distro!
---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt install python3 default-jre gcc valgrind neofetch htop vim git texlive-full
+
+git clone git@github.com:briancpark/cs61a.git cs61a
+git clone git@github.com:briancpark/cs70.git cs70
+
+git clone git@github.com:briancpark/cs170.git cs170/hw
+git clone git@github.com:briancpark/cs188.git cs188
+
