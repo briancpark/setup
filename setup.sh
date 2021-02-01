ec3de912d0f8699c95ef8611111907f0064c23a9
Brian Park <briancpark@berkeley.edu>
2021-02-01 09:45:09 -0500

    add latex package

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo apt install python3 default-jre gcc valgrind neofetch htop vim git texlive-full pip3-python libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+sudo apt install python3 default-jre gcc valgrind neofetch htop vim git texlive-full pip3-python libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 texlive-fonts-extra
 
 git clone git@github.com:briancpark/cs61a.git cs61a
 git clone git@github.com:briancpark/cs70.git cs70
