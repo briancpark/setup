0fb27eb7be0fec6ed16271cf1c2abfd8b0b55223
Brian Park <briancpark@berkeley.edu>
2021-06-03 00:03:58 -0400

    update script to work on ubuntu

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo apt install python3 default-jre gcc valgrind neofetch htop vim git texlive-full pip3-python libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 texlive-fonts-extra -y
+sudo apt install python3 default-jre gcc valgrind neofetch htop vim git texlive-full libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 texlive-fonts-extra -y
 
 git clone git@github.com:briancpark/cs61a.git cs61a
 git clone git@github.com:Berkeley-CS61B-Student/su20-540491-g6.git cs61bl/archive
@@ -25,6 +25,8 @@ mv vimrc .vimrc
 
 wget https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
 bash Anaconda3-2021.05-Linux-x86_64.sh
+
+export PATH="~/anaconda3/bin:$PATH"
 
 conda create -n cs61a python=3.8
 conda create -n cs188 python=3.6
