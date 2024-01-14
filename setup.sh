16b2f530fab5a8d826e85ff86574e85d83c4c8ab
Brian Park <bcpark@ncsu.edu>
2024-01-14 12:42:46 -0500

    Update and clean setup script

--- setup.sh
+++ setup.sh
@@ -2,37 +2,85 @@
 
 # Install OS-specific packages
 
+### LINUX ###
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    sudo apt install python3 python3-pip default-jre gcc valgrind neofetch htop vim git texlive-full libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 texlive-fonts-extra xclip tmux vlc nmap snap -y    
+    sudo apt install \
+        python3 \
+        python3-pip \
+        default-jre \
+        gcc \
+        valgrind \
+        neofetch \
+        htop \
+        vim \
+        git \
+        texlive-full \
+        libgl1-mesa-glx \
+        libegl1-mesa \
+        libxrandr2 \
+        libxrandr2 \
+        libxss1 \
+        libxcursor1 \
+        libxcomposite1 \
+        libasound2 \
+        libxi6 \
+        libxtst6 \
+        texlive-fonts-extra \
+        xclip \
+        tmux \
+        vlc \
+        nmap \
+        snap \
+        -y    
     
-    # External installations
+    ### External Installations ###
+    # Google Chrome
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo apt install ./google-chrome-stable_current_amd64.deb
     rm google-chrome-stable_current_amd64.deb
 
-    # Anaconda configuration
+    # Anaconda 
     wget -O - https://www.anaconda.com/distribution/ 2>/dev/null | sed -ne 's@.*\(https:\/\/repo\.anaconda\.com\/archive\/Anaconda3-.*-Linux-x86_64\.sh\)\">64-Bit (x86) Installer.*@\1@p' | xargs wget
     
-
     # Other installation
     sudo snap install --classic code 
     sudo snap install --classic heroku
+
+### MAC OS ###
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Install Homebrew
+    touch ~/.hushlogin
     if ! [ -x "$(command -v brew)" ]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"    
     fi
     
 
-    # Apple Silicon
+    ### Apple Silicon ###
     if [[ $(uname -m) == 'arm64' ]]; then
-        true
-    # Intel
+        # Make sure native Homebrew for ARM is in path
+        echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+
+    ### Intel ###
     elif [[ $(uname -m) == 'x86_64' ]]; then
         wget -O - https://www.anaconda.com/distribution/ 2>/dev/null | sed -ne 's@.*\(https:\/\/repo\.anaconda\.com\/archive\/Anaconda3-.*-Linux-MacOS\.sh\)\">64-Bit (x86) Installer.*@\1@p' | xargs wget
     fi
 
-    brew install vim git gcc valgrind neofetch htop tmux vlc nmap vagrant golangci-lint clang-format wget cmake
+    brew install \
+        vim \
+        git \
+        gcc \
+        valgrind \
+        neofetch \
+        htop \
+        tmux \
+        vlc \
+        nmap \
+        vagrant \
+        golangci-lint \
+        clang-format \
+        wget \
+        cmake
+    
 else
     error "Unknown OS type: $OSTYPE"
 fi
@@ -49,12 +97,13 @@ if ! [ -f ~/.ssh/id_ed25519.pub ]; then
     read response
 fi
 
-# Git configuration
+
+
+### Git Repositories ###
 git config --global user.name "Brian Park"
 git config --global user.email bcpark@ncsu.edu
 git config --global core.editor vim
 
-# Clone UC Berkeley Repositories
 git clone --recurse git@github.com:briancpark/cs61a.git cs61a
 git clone --recurse git@github.com:briancpark/cs61bl.git cs61bl
 git clone --recurse git@github.com:briancpark/cs61c.git cs61c
@@ -69,21 +118,14 @@ git clone --recurse git@github.com:briancpark/cs170.git cs170
 git clone --recurse git@github.com:briancpark/cs188.git cs188
 git clone --recurse git@github.com:briancpark/cs189.git cs189
 git clone --recurse git@github.com:briancpark/cs267.git cs267
-
-# Clone NCSU Repositories
-git clone --recurse git@github.com:briancpark/csc512.git
-git clone --recurse git@github.com:briancpark/csc561.git
-git clone --recurse git@github.com:briancpark/csc591-007.git
-git clone --recurse git@github.com:briancpark/csc591-026.git
-git clone --recurse git@github.com:briancpark/csc791-025.git
-git clone --recurse git@github.com:briancpark/csc766.git
-git clone --recurse git@github.com:briancpark/ece786.git
-
-git clone git@github.com:briancpark/vim.git
-cp vim/vimrc ./
-mv vimrc .vimrc
-rm -rf vim
-
+git clone --recurse git@github.com:briancpark/csc512.git csc512
+git clone --recurse git@github.com:briancpark/csc542.git csc542
+git clone --recurse git@github.com:briancpark/csc561.git csc561
+git clone --recurse git@github.com:briancpark/csc591-007.git csc591-007
+git clone --recurse git@github.com:briancpark/csc591-026.git csc591-026
+git clone --recurse git@github.com:briancpark/csc791-025.git csc791-025
+git clone --recurse git@github.com:briancpark/csc766.git csc766
+git clone --recurse git@github.com:briancpark/ece786.git ece786
 
 # Install Anaconda
 bash Anaconda3*.sh
@@ -101,14 +143,25 @@ conda create -n cs189 python=3.8.5 -y
 conda create -n eecs16a python=3.8 -y
 conda create -n eecs16b python=3.8 -y
 
+conda create -n csc542 python=3.10 -y
 conda create -n csc591 python=3.8 -y
 conda create -n csc791 python=3.10 -y
 
 conda create -n nums python=3.7 -y
 
-cd ds100
-conda env create -f data100_environment.yml
-cd ../cs189
-pip3 install -r requirements.txt
 
+### Vim Configuration ###
+git clone https://github.com/powerline/fonts
+cd fonts
+./install.sh
 
+# Vim Configuration
+git clone git@github.com:briancpark/vim.git
+cp vim/vimrc ./
+mv vimrc .vimrc
+rm -rf vim
+
+git clone https://github.com/github/copilot.vim \
+   ~/.vim/pack/github/start/copilot.vim
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
