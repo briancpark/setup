8697bfa4f4a7085053b35bd4a95a0c0fc792cd4d
Brian Park <me@briancpark.com>
2025-08-12 22:01:55 -0700

    copy .zshrc correctly

--- setup.sh
+++ setup.sh
@@ -170,6 +170,8 @@ conda_setup() {
 # Setup Script
 ###############
 
+cp .zshrc "$HOME/.zshrc"
+
 # We start and install everything in the home directory
 cd $HOME
 
@@ -190,7 +192,6 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     fi
     sudo chsh -s $(which zsh)
-    cp .zshrc "$HOME/.zshrc"
 
     if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
         git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
